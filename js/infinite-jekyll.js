f9a86c24edc8cd9c8346bcb34cfed523108f2903
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2014-10-29 09:19:35 +0100

    Fix dark-overlay in footer and fix lazy scrolling

--- js/infinite-jekyll.js
+++ js/infinite-jekyll.js
@@ -1,3 +1,5 @@
+---
+---
 $(function() {
   
   var postURLs,
@@ -7,7 +9,7 @@ $(function() {
       loadNewPostsThreshold = 3000;
   
   // Load the JSON file containing all URLs
-  $.getJSON('/all-posts.json', function(data) {
+  $.getJSON("{{ site.baseurl }}/all-posts.json", function(data) {
     postURLs = data["posts"];
     
     // If there aren't any more posts available to load than already visible, disable fetching
